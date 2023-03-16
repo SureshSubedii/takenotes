a49521b6f8cac3836c6c19eb8f2f83f9c1bff11a
SureshSubedii <sureshsubedi485@gmail.com>
2023-03-16 20:25:07 +0545

    Project initialization

---
+++ src/Notes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import clip from './clipB.png'
+
+function Notes() {
+  return (
+    <div className="notes">
+        <img src={clip} alt="Img Loading"/>
+    </div>
+  )
+}
+
+export default Notes
