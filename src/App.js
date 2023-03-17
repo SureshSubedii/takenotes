61a6d67bc5862f72cf6343a951b267e62710166b
SureshSubedii <sureshsubedi485@gmail.com>
2023-03-17 12:50:18 +0545

    Navbar and Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 import Notes from './Notes';
 
 function App() {
   return (
     <div className="app">
+      <Navbar/>
      <Notes/>
     </div>
   );
--- src/Notes.js
+++ src/Notes.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import clip from './clipB.png'
 import './Notes.css'
+import {useForm} from 'react-hook-form'
 
 function Notes() {
+  const {handleSubmit,register,formState:{errors}}=useForm();
+  const submit=(data)=>{
+    console.log(data)
+
+  }
   return (
     <div className="notes" >
-        {/* <img src={clip} alt="Img Loading"/> */}
         
-        <form>
-            <input className="notes_title" placeholder='Add title'></input>
-            <textarea placeholder="Add description"></textarea>
+        <form onSubmit={handleSubmit(submit)}>
+            <input type="text" className="notes_title" placeholder='Add title'{...register("title", { required: "Required!" })}  >
+            
+            </input>
+            {errors.title && <p className="p1" style={{marginLeft:'380px',marginTop:'-25px',color:'red'}}> {errors.title.message}</p> }
+            <textarea  type="text" placeholder="Add description" {...register("description", { required: "Required!" })}></textarea>
+            {errors.description && <p  className="p2" style={{marginLeft:'380px',marginTop:'-80px',color:'red'}}> {errors.description.message}</p>}
+            <button type="submit" >Save Note</button>
 
         </form>
     </div>
